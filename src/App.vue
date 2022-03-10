74b8edf1a50c9865edba1ceeab3efbedb79a21cd
2216568135@qq.com <2216568135@qq.com>
2022-03-10 14:57:09 +0800

    项目初始化，引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <h1>西南开源</h1>
     <router-view />
+    <h1>西南开源</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
+
 <script>
 export default {
   name: 'App'
 }
 </script>
+
 <style scoped lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import './styles/index.less'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
