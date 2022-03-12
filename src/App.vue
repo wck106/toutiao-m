70d58693e137398b8aca64c8cb5434d8915b1913
2216568135@qq.com <2216568135@qq.com>
2022-03-12 10:38:29 +0800

    增加登录功能

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
     <router-view />
-    <h1>西南开源</h1>
-    <div>
-      <i class="toutiao toutiao-shipin"></i>
-    </div>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="login-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar" title="登录" />
+    <!-- /导航栏 -->
+
+    <!-- 登录表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="user.mobile"
+        name="手机号"
+        placeholder="请输入手机号"
+        :rules="userFormRules.mobile"
+        type="number"
+        maxlength="11"
+      >
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+      <van-field
+        v-model="user.code"
+        name="验证码"
+        placeholder="请输入验证码"
+        :rules="userFormRules.code"
+        type="number"
+        maxlength="6"
+      >
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <!--
+            time: 倒计时时间
+          -->
+          <van-count-down
+            v-if="isCountDownShow"
+            :time="1000 * 10"
+            format="ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            class="send-sms-btn"
+            native-type="button"
+            round size="small"
+            type="default"
+            @click="onSendSms"
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+      <div class="login-btn-wrap">
+        <van-button class="login-btn" block type="info" native-type="submit">
+          登录
+        </van-button>
+      </div>
+    </van-form>
+    <!-- /登录表单 -->
+  </div>
+</template>
+
+<script>
+import { login } from '@/api/user'
+
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      userFormRules: {
+        mobile: [{
+          required: true,
+          message: '手机号不能为空'
+        }, {
+          pattern: /^1[3|5|7|8]\d{9}$/,
+          message: '手机号格式错误'
+        }],
+        code: [{
+          required: true,
+          message: '验证码不能为空'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '验证码格式错误'
+        }]
+      },
+      isCountDownShow: false // 是否展示倒计时
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {
+    async onSubmit (values) {
+      // 1. 获取表单数据
+      const user = this.user
+
+      // TODO: 2. 表单验证
+
+      // 在组件中必须通过 this.$toast 来调用 Toast 组件
+      this.$toast.loading({
+        message: '登陆中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0
+      })
+      // 3. 提交表单登录请求
+      try {
+        const res = await login(user)
+        console.log('登陆成功', res)
+        this.$toast.success('登陆成功')
+      } catch (err) {
+        if (err.response.status === 400) {
+          this.$toast.fail('手机号或验证码错误')
+        } else {
+          this.$toast.fail('登陆失败，请稍后重试')
+        }
+      }
+
+      // 4. 根据请求响应结果处理后续操作
+    },
+
+    async onSendSms () {
+      console.log('onSendSms')
+      // 1. 校验手机号
+      try {
+        await this.$recfs.loginForm.validate('mobile')
+        console.log('验证通过')
+      } catch (err) {
+        console.log('验证失败', err)
+      }
+      // 2. 验证通过，显示倒计时
+      this.isCountDownShow = true
+      // 3. 请求发送验证码
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+  .send-sms-btn {
+    width: 152px;
+    height: 46px;
+    line-height: 46px;
+    background-color: #ededed;
+    font-size: 22px;
+    color: #666;
+  }
+  .login-btn-wrap {
+    padding: 53px 33px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+    }
+  }
+}
+</style>
