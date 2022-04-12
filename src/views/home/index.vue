05dacca9ae29011ad9f8a0cd34d6a3a89445fe7e
2216568135@qq.com <2216568135@qq.com>
2022-04-12 22:36:58 +0800

    首页基本结构

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button
+        class="search-btn"
+        slot="title"
+        type="info"
+        size="small"
+        round
+        icon="search"
+      >搜索</van-button>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage',
+  name: 'HomeIndex',
   components: {},
   props: {},
   data () {
@@ -18,4 +31,20 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.home-container {
+  .van-nav-bar__title {
+    max-width: unset;
+  }
+  .search-btn {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+    border: none;
+    font-size: 28px;
+    .van-icon {
+      font-size: 32px;
+    }
+  }
+}
+</style>
